4eaac0891782b290fce07fa7971e7ea8e0f39c1b
Quemo <coolatme@gmx.net>
2023-01-28 21:44:40 +0100

    Final commit

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -2,40 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-#if UNITY_EDITOR
+using UnityEngine.UI;
 using UnityEditor;
-#endif
+using TMPro;
 
-// Sets the script to be executed later than all default scripts
-// This is helpful for UI, since other things may need to be initialized before setting the UI
-[DefaultExecutionOrder(1000)]
 public class MenuUIHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //This is the handler of the main menu scene
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] TextMeshProUGUI PlayerNameInput;
 
-    public void StartNew()
+    public void StartGame()
     {
         SceneManager.LoadScene(1);
     }
 
-    public void Exit()
+    public void SetPlayerName()
     {
-        MainManager.Instance.SaveColor();
+        PlayerDataHandler.Instance.PlayerName = PlayerNameInput.text;
+    }
 
+    public void ExitGame()
+    {
 #if UNITY_EDITOR
         EditorApplication.ExitPlaymode();
 #else
-        Application.Quit(); // original code to quit Unity player
+        Application.Quit();
 #endif
     }
 }
